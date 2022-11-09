7e5d01b29cbb83d4ec80bafaaee717cc43af7f3f
https://github.com/Sonaljagani/Sonaljagani.git <sonal.patel415@gmail.com>
2022-11-09 16:44:05 +0530

    New code

---
+++ src/App.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react'
+import './App.css'
+// import { ClickCounter } from './compomnents/ClickCounter'
+// import Login from './compomnents/Login'
+import NewRegform from './compomnents/NewRegform'
+// import RegistrationForm from './compomnents/RegistrationForm'
+// import './compomnents/appStyle.css' 
+// import style from './compomnents/appStyle.module.css'
+// import PostList from './compomnents/PostList'
+// import PostForm from './compomnents/PostForm'
+// import DataFachinghooks from './compomnents/DataFachinghooks'
+// import ParentComponent from './compomnents/My app/ParentComponent'
+
+
+
+ class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        {/* <ClickCounter/> */}
+        {/* <Login/>  */}
+        {/* <RegistrationForm/> */}
+         <NewRegform/> 
+        {/* <PostList/> */}
+        {/* <PostForm/> */}
+        {/* <DataFachinghooks/> */}
+
+        {/* <ParentComponent/> */}
+        
+      </div>
+    )
+  }
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+// import Form from './compomnents/Form'
+// import ParentComp from './compomnents/ParentComp'
+// import PureComp from './compomnents/PureComp'
+// import Table from './compomnents/Table'
+
+// import { Greet } from './compomnents/Greet';
+// import { Welcome} from './compomnents/welcome';
+// //import  Welcome from './compomnents/welcome';
+// import  {Hello} from './compomnents/Hello';
+// import { Message } from './compomnents/message';
+// import { Counter } from './compomnents/Counter';
+// import { Functionclick } from './compomnents/Functionclick';
+// import { Classclick } from './compomnents/Classclick';
+// import { EventBind } from './compomnents/EventBind';
+// import {ParentComponent} from './compomnents/ParentComponent';
+// import {UserGreeting} from './compomnents/UserGreeting';
+// import { NameList } from './compomnents/NameList';
+// import {Stylesheet}  from './compomnents/Stylesheet';
+// import { Inline } from './compomnents/Inline';
+// import './compomnents/appStyle.css'
+// import style from './compomnents/appStyle.module.css'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+      {/* <h1 className={style.Sucess}>Sucess</h1>
+      <Inline/> */}
+      {/* <Stylesheet primary={false}/> */}
+      {/* <NameList/> */}
+      {/* <UserGreeting/> */}
+      {/*<ParentComponent></ParentComponent>*/}
+      {/*<EventBind></EventBind>*/}
+      {/*<Functionclick/>*/}
+      {/*<Classclick/>*/}
+      {/*<Counter/>*/}
+      {/*<Message></Message>*/}
+     {/*<Greet name='Krishiv' nikename='ganu'>
+        <p>This is Children compomnents</p>
+      </Greet>
+      <Greet name='Dhyana' nikename='dyanukdi'>
+        <button>Click</button>
+      </Greet>
+      <Greet name='Henil' nikename='henu' />
+      <Greet name='pranav' nikename='pranu' />
+      <Welcome name='Henil' nikename='henu'></Welcome>
+    <Welcome name='pranav'nikename='pranu'></Welcome>*/}
+  {/* <Hello></Hello> */}
+      {/* <Form/> */}
+        {/* <Table/> */}
+        // <ParentComp/>
